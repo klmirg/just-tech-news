7a4d61d533d8cb72b80ed6537de60619c4237af5
Ronya <klmirg6@gmail.com>
2022-01-01 03:10:30 -0600

    finished the issue

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -61,8 +61,14 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
-    res.json({ user: dbUserData });
-
+    
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 
